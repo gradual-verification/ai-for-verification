5a3c5694fbf646301138ceca01df55ff3dd03273
ruizhaonini1 <74476225+ruizhaonini1@users.noreply.github.com>
2024-08-16 18:13:51 -0400

    Update fractions-counting_w.c

--- mimic-user-behavior-input/fractions-counting_z/fractions-counting_w.c
+++ mimic-user-behavior-input/fractions-counting_z/fractions-counting_w.c
@@ -16,27 +16,7 @@ struct C* create_C(int x)
   return c;
 }
 
-/*@
-predicate counter(struct C* c, int x);
 
-predicate ticket(struct C* c);
-
-lemma void create_counter(struct C* c);
-  requires c->x |-> ?x;
-  ensures counter(c, x);
-
-lemma void create_ticket(struct C* c);
-  requires counter(c, ?x);
-  ensures counter(c, x) &*& ticket(c) &*& c->x |-> x;
-
-lemma void dispose_ticket(struct C* c);
-  requires counter(c, ?x) &*& ticket(c) &*& c->x |-> x;
-  ensures counter(c, x);
-
-lemma void dispose_counter(struct C* c);
-  requires counter(c, ?x);
-  ensures c->x |-> x;
-@*/
 
 
 
