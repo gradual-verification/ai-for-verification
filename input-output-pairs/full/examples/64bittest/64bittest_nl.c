0e28c2589fe8d7b576fd01a4630c8bc05f0d0b66
sanyadod <85787361+sanyadod@users.noreply.github.com>
2025-02-17 14:29:56 -0500

    Add files via upload

---
+++ input-output-pairs/full/examples/64bittest/64bittest_nl.c
@@ -0,0 +1,10 @@
+/***
+ * Description:
+ * The `test` function verifies that the size of a pointer is 8 bytes.
+ *
+ * @param none - This function does not take any parameters.
+ */
+void test()
+{
+    assert(sizeof(void *) == 8);
+}
