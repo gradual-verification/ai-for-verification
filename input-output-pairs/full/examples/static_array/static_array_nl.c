e0a392e2bd215a2b66afce2af22f1dace438901e
marilynjrego <91641188+marilynjrego@users.noreply.github.com>
2025-02-26 01:41:16 -0500

    Create static_array_nl.c

---
+++ input-output-pairs/full/examples/static_array/static_array_nl.c
@@ -0,0 +1,162 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+
+#include <stdbool.h>
+
+/*check() function
+-params: boolean value
+-description: the function asserts the boolean value*/
+void check (bool b)
+{
+  assert(b);
+}
+
+typedef struct
+ {
+  int x;
+  int ar [7];
+  int y;
+ } struct_with_array;
+
+/*check_local_inits() function
+-params: two integer arguments
+-description: This function initializes a local struct, a local struct pointer, and a local array,
+and then checks if the local struct, local struct pointer, and local array are initialized correctly.*/
+void check_local_inits(int x, int y)
+{
+  struct_with_array foo = {123, {2, x, 5, 7, 11, 13, y}, 456};
+  struct_with_array bar = foo;
+  char buf[3] = {1, 2, 3};
+  
+  check((&foo)->x == 123);
+  check((&foo)->ar[6] == 17);
+  check(buf[1] == 2);
+}
+
+struct mystruct {
+  struct_with_array s1;
+  int s2;
+};
+
+
+struct mystruct my_global_nested_struct = {{42, {420, 421, 422, 423, 424, 425, 426}, -3}, -99};
+
+/*foo() function
+-params: none
+-description: This function initializes a local struct and a local struct pointer, 
+and then checks if the global struct is different from the local struct and the local struct pointer*/
+static void foo()
+{
+  struct mystruct my_local_nested_struct;
+  memset(&my_local_nested_struct, 0, sizeof(struct mystruct));
+  memset(&(&my_local_nested_struct)->s1, 0, sizeof(struct_with_array));
+  assert(&my_global_nested_struct != &my_local_nested_struct);
+  struct mystruct *sh = malloc(sizeof(struct mystruct));
+  if (sh == 0) abort();
+  assert(sh != &my_global_nested_struct);
+  assert(sh != &my_local_nested_struct);
+  (&(&my_global_nested_struct)->s1)->ar[5] = 100;
+  (&(&my_local_nested_struct)->s1)->ar[5] = 200;
+  (&sh->s1)->ar[5] = 300;
+  free(sh);
+}
+
+static int ar2 [55];
+
+void mod_ar2 (void)
+ {
+  ar2[ 1] = ar2[ 1] + ar2[26];
+  return;
+ }
+
+static struct_with_array bigArray[10] = {{100, {1,2,3,4}, 200}, {300, {5,6,7}, 400}}; // Incomplete initializer lists; remaining elements get default value.
+
+struct point { int x; int y; };
+
+struct point points[] = { { 10, 20 }, { 30, 40 } };
+
+
+/*main() function
+-params: an integer arguement and a character pointer argument
+-description: This function initializes and utilizes a global struct, a global struct pointer, a global array, 
+a local struct, a local struct pointer, a local array, and two local integers.
+*/
+int main(int argc, char **argv) //@ : main_full(static_array)
+ {
+  check((&(&my_global_nested_struct)->s1)->x == 42);
+  check((&(&my_global_nested_struct)->s1)->ar[0] == 420);
+  check((&(&my_global_nested_struct)->s1)->ar[6] == 426);
+  check((&(&my_global_nested_struct)->s1)->y == -3);
+  check((&my_global_nested_struct)->s2 == -99);
+  
+  struct_with_array *bigArrayPtr = bigArray;
+  check((bigArrayPtr + 1)->x == 300);
+  check((bigArrayPtr + 1)->ar[2] == 7);
+  
+  foo();
+
+  struct_with_array *s;
+  int    i = 1;
+  int    ar1 [55];
+  int    t;
+
+  /* normal array */
+  ar1[ 0] = 1;
+  ar1[ 1] = 5;
+  ar1[ 2] = 0;
+  ar1[26] = 2;
+  ar1[ 1] = ar1[ 1] + ar1[26];
+
+  if (ar1[i] == 7)
+   { t = ar1[2]; }
+   else
+   { assert false; }
+
+  assert (ar1[26] == 2);
+
+  /* array inside a struct */
+  s = malloc (sizeof (struct_with_array));
+  if (s == 0) { abort(); }
+
+  s->ar[ 0] = 1;
+  s->ar[ 1] = 5;
+  s->ar[ 2] = 0;
+  s->ar[ 6] = 2;
+  s->ar[ 1] = s->ar[ 1] + s->ar[ 6];
+
+  if (s->ar[i] == 7)
+   { t += s->ar[2]; }
+   else
+   { assert false; }
+
+  assert (s->ar[0] == 1);
+  free (s);
+
+
+  /* global array */
+  check(ar2[0] == 0);
+  ar2[ 0] = 1;
+  ar2[ 1] = 5;
+  ar2[ 2] = 0;
+  ar2[26] = 2;
+  mod_ar2 ();
+
+  if (ar2[i] == 7)
+   { t += ar2[2]; }
+   else
+   { assert false; }
+
+  assert (ar2[1] == 7);
+
+  assert (points[1].y == 40);
+  
+  int xs[] = {1, 2, 3}, ys[] = {4, 5, 6, 7};
+  xs[1] = xs[2];
+  assert (xs[1] == 3);
+  ys[2] = ys[3];
+  assert (ys[2] == 7);
+
+  return (t);
+ }
